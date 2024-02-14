24514f3a7aa0ed468b87fd8cb9f8c975e8944ec3
Muhammad Faks <muhammad.faks@gmail.com>
2024-02-14 10:25:30 -0800

    Added game filed

---
+++ Cave-Exploration-Adventure/wumpus.h
@@ -0,0 +1,30 @@
+/******************************************************
+** Program: wumpus.h
+** Author: Muhammad Faks
+** Description: Inhereted polymorphically from event class
+** Input: n/a
+** Output: Uses polymorphism to output the wumpus precept and enounter message
+******************************************************/
+
+#ifndef WUMPUS_H
+#define WUMPUS_H
+
+#include <iostream>
+#include <string>
+
+#include "event.h"
+
+using namespace std;
+
+class Wumpus : public Event {
+	protected:
+	
+	public:
+		Wumpus(); //default constructor
+		void precept(); //precept function
+		void encounter(); //encouter function
+		char event_letter(); //designating event letter function 
+
+};
+
+#endif
